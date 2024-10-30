30767628b310008986081bd2ea0a07f206589565
djangouser <ngn2349566@stu.o-hara.ac.jp>
2024-10-30 10:16:43 +0900

    ペット登録

--- petapp/admin.py
+++ petapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Person
+from .models import Pet
 
-admin.site.register(Person)
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('type', 'size', 'color', 'age')  # nameを削除
+    search_fields = ('type', 'size', 'color', 'age')  # 検索機能を追加
--- petapp/models.py
+++ petapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 
+class Pet(models.Model):
+    TYPE_CHOICES = [
+        ('dog', '犬'),
+        ('cat', '猫'),
+    ]
+
+    SIZE_CHOICES = [
+        ('large', '大型'),
+        ('medium', '中型'),
+        ('small', '小型'),
+    ]
+
+    id = models.IntegerField(primary_key=True)  # 明示的にIDフィールドを定義
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='unknown')
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES, default='unknown')
+    color = models.CharField(max_length=100, default='', blank=True)
+    age = models.IntegerField()
+    image = models.ImageField(upload_to='pets/', null=True, blank=True)  # 画像フィールドを追加
+
+    def __str__(self):
+        return f'{self.type} - {self.size} - {self.color} - {self.age}歳'
+
+
+class Survey(models.Model):
+    # 例：アンケートで収集する情報
+    pet_type = models.CharField(max_length=10, choices=[('dog', '犬'), ('cat', '猫')])
+    size = models.CharField(max_length=10, choices=[('small', '小型'), ('medium', '中型'), ('large', '大型')])
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.pet_type} - {self.size} - {self.age}"
