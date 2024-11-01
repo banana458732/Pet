3769f1f943d5feb1e6713543d66ff9e8ec74dff9
djangouser <ngn2349566@stu.o-hara.ac.jp>
2024-11-01 10:46:26 +0900

    index

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import index
+
+app_name = 'messaging'
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('messaging/', views.IndexView.as_view(), name='send_message'),
+    path('', index, name='index'),
     path('send/', views.send_message, name='send_message'),
     path('pets/<str:pet_id>/', views.pet_detail, name='pet_detail'),  # ペットの詳細
 ]
