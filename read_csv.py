175e6eac25ecef0b9206c557eb0e6aa98ab8c9f4
djangouser <ngn2349566@stu.o-hara.ac.jp>
2024-11-13 11:01:59 +0900

    パス変更

---
+++ read_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# CSVファイルのパスを指定
+csv_file_path = 'C:\\Users\\t_koitabashi\\Desktop\\卒業制作\\Pet\\pets_data.csv'
+
+# CSVファイルを読み込む
+data = pd.read_csv(csv_file_path)
+
+# データの先頭5行を表示
+print(data.head())
