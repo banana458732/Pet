b1f4d1fd7a7e8f5085255a850baa9b135290d9a1
三宅翔大 <ngn2349526@stu.o-hara.ac.jp>
2024-11-18 11:23:18 +0900

    アンケートのCSS

--- Survey/forms.py
+++ Survey/forms.py
@@ -5,12 +5,9 @@ class SimplePetSurveyForm(forms.Form):
     # 全ての情報を一つのフィールドに集約した自由入力フィールド
     general_info = forms.CharField(
         widget=forms.Textarea(attrs={'rows': 5, 'cols': 50}),
-        label='求めているペットについて自由にご記入ください。',
-        help_text='（例: ペットの種類、性格、性別など)',
-        required=True  # 必須項目
+        required=True
     )
 
-    # 年齢の範囲を指定するチェックボックスフィールド
     AGE_CHOICES = [
         ('0-3', '0~3歳'),
         ('4-7', '4~7歳'),
@@ -20,6 +17,5 @@ class SimplePetSurveyForm(forms.Form):
     age_range = forms.MultipleChoiceField(
         choices=AGE_CHOICES,
         widget=forms.CheckboxSelectMultiple,
-        label='希望するペットの年齢範囲(任意)',
-        required=False  # 必須ではない
+        required=False
     )
