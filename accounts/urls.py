9e5fb9aa7d7ecdbdd04c4c0f62b2be58fab04ef3
tanaka <ngn2349544@stu.o-hara.ac.jp>
2024-11-20 14:47:00 +0900

    ログイン完了画面削除

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,15 +19,11 @@ urlpatterns = [
          name='login'
          ),
 
-    path('login_success/',
-         views.LoginSuccessView.as_view(template_name='accounts/login_success.html'),
-         name='login_success'
-         ),
-
     path('logout/',
          auth_views.LogoutView.as_view(template_name='accounts/logout.html'),
          name='logout'
          ),
+         
     path('',
          views.IndexView.as_view(template_name='accounts/index.html'),
          name='index')
