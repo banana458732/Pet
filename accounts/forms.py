ac5fe7197bb18bd732760473b06e591ea210c5bc
tanaka <ngn2349544@stu.o-hara.ac.jp>
2024-11-26 11:33:36 +0900

    アカウント登録修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class CustomUserCreationForm(UserCreationForm):
     address = forms.CharField(label="住所", max_length=31, required=True)
     phone_number = forms.CharField(label='電話番号', max_length=15, required=True)
-
+    
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password1', 'password2','address','phone_number')
